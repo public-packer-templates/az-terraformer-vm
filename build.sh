2da37964b9770d780f12354544386ebc282df1cc
Your Name <you@example.com>
2019-08-11 05:51:14 +0000

    support image gallery

--- build.sh
+++ build.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-## Install git
-sudo yum -y install git
-
-## Install ansible
-sudo yum -y install epel-release python-devel openssl-devel gcc
-sudo yum -y install python-pip
-sudo pip  install --upgrade pip
-sudo pip  install ansible
-
 ## Install necessary ansible roles for provision
 git clone git@github.com:public-ansible-playbooks/terraformer.git ./ansible
 ansible-galaxy install -r ./ansible/requirements.yml -p ./ansible/roles
